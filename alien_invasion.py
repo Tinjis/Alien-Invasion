5805439be7c1b31688cc506b9ef28f8541fea174
cybertrojan <thecybertrojan@gmail.com>
2025-04-25 22:25:22 +0100

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     
     def __init__(self):
         pygame.init()
-    
-        self.screen = pygame.display.set_mode((800, 400))
-    
+
+        self.settings = Settings()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        
     def run_game(self):
 
         while True:
@@ -15,7 +20,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
+            
 
 if __name__ == "__main__":
     alien_invasion = AlienInvasion()
